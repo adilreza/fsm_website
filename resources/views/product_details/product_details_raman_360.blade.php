38b7f221d4c4fbcf1f16e06bbcba03f410fb09b0
Adil Reza <adilreza043@gmail.com>
2019-01-31 01:09:23 +0600

    fixing raman

--- resources/views/product_details/product_details_raman_360.blade.php
+++ resources/views/product_details/product_details_raman_360.blade.php
@@ -117,7 +117,7 @@
                     <hr style="width:80%;background:darkblue;">
                     <div class="latest_post_wrapper">
                         <div class="blog_wrapper1">
-                            <div class="sc_blog_image"> <img alt="blog_img1" class="img-responsive" style="height:80px;width:80px;" src="{{URL::asset('fsm_all_web_file/fsm_image_gallery/allproducts/fsm_product_127_C2C.PNG')}}"> </div>
+                            <div class="sc_blog_image"> <img alt="blog_img1" class="img-responsive" style="height:80px;width:80px;" src="{{URL::asset('fsm_all_web_file/fsm_image_gallery/allproducts/fsm_product_127_C2C.png')}}"> </div>
                             <div class="sc_blog_text">
                                 <h5><a href="{{URL::to('/product_127_C2C')}}">127 C2C</a></h5>
                                 <div class="sc_blog_date">June 28, 2018</div>
