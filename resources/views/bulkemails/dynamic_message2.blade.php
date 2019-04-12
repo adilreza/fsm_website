f784b842b46ebe0df771e74c2a7777e8ad07e2f8
Adil Reza <adilreza043@gmail.com>
2019-04-12 23:51:39 +0600

    testig2

--- resources/views/bulkemails/dynamic_message2.blade.php
+++ resources/views/bulkemails/dynamic_message2.blade.php
@@ -1,4 +1,4 @@
 
 <h3>This is newsletter boss</h3>
 <h3>{{$file_id}}</h3>
-<iframe style="height:100%;width:100%;margin:0 auto;" id="newsletter_preview" src="http://webtest.fsmtranscriber.com/admin/news_letter_api/{{$file_id}}"></iframe>
+<iframe style="height:700px;width:700px;margin:0 auto;overflow:scroll;" id="newsletter_preview" src="http://webtest.fsmtranscriber.com/admin/news_letter_api/{{$file_id}}"></iframe>
