943367bb99989f4803a128c6fe227ac9eae1cde2
Adil Reza <adilreza043@gmail.com>
2019-04-13 00:05:13 +0600

    test4

--- resources/views/bulkemails/dynamic_message2.blade.php
+++ resources/views/bulkemails/dynamic_message2.blade.php
@@ -2,10 +2,20 @@
 <html>
 <body>
 
-<h2>HTML Iframes</h2>
-<p>You can also use the CSS height and width properties to specify the size of the iframe:</p>
+<h2>Iframe - Target for a Link</h2>
 
-<iframe src="https://www.w3schools.com/bootstrap4/default.asp" style="height:600px;width:800px"></iframe>
+<iframe height="300px" width="100%" src="demo_iframe.htm" name="iframe_a"></iframe>
+
+<p><a href="https://www.w3schools.com" target="iframe_a">W3Schools.com</a></p>
+
+<p>When the target of a link matches the name of an iframe, the link will open in the iframe.</p>
+
+<div style="color:red;height:400px;width:500px;">
+    <h1>This is div test</h1>
+    <h1>This is div test</h1>
+    <h1>This is div test</h1>
+    <h1>This is div test</h1>
+</div>
 
 </body>
 </html>
