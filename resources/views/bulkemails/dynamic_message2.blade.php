e5fc8f0ff186d295d586456cbc353499b68da2a3
Adil Reza <adilreza043@gmail.com>
2019-04-12 23:46:59 +0600

    addin both id and test for test

--- resources/views/bulkemails/dynamic_message2.blade.php
+++ resources/views/bulkemails/dynamic_message2.blade.php
@@ -1,2 +1,4 @@
 
+<h3>This is newsletter boss</h3>
+<h3>{{$file_id}}</h3>
 <iframe style="height:100%;width:100%;margin:0 auto;" id="newsletter_preview" src="http://webtest.fsmtranscriber.com/admin/news_letter_api/{{$file_id}}"></iframe>
