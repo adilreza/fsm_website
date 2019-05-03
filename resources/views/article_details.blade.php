2cba11ea687ffce1b1c96cf6e3d06603c28ce744
Adil Reza <adilreza043@gmail.com>
2019-05-03 15:45:29 +0600

    adding background image

--- resources/views/article_details.blade.php
+++ resources/views/article_details.blade.php
@@ -13,21 +13,30 @@
 
 
 <!-- Inner Page Wrapper Start -->
-<div class="inner-page-wrapper search-results-wrapper" style="padding:50px 10px;">
+<div class="inner-page-wrapper search-results-wrapper" style="padding-bottom:30px">
     @foreach ($single_article as $article)
         
-   
-    <div class="container">
-    <h3 style="color:darkturquoise;">{{$article->post_title}}</h3>
-        <hr>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 text-center">
-                    <img src="{{URL::asset('fsm_all_web_file/fsm_image_gallery/library_image/'.$article->display_image)}}" style="height:250px;width:340px;">
+   <div style="background-image: linear-gradient(to bottom right, #3c40c6, white)">
+        <div class="container" style="padding:30px 0px;;" >
+            <div class="row">
+                <div class="col-md-7">
+                    <h1 style="color:white;">{{$article->post_title}}</h1>
+                    
+                </div>
+            <div class="col-md-5">
+                    <div class="row">
+                        <div class="col-md-4 text-center">
+                                <img src="{{URL::asset('fsm_all_web_file/fsm_image_gallery/library_image/'.$article->display_image)}}" style="height:260px;width:340px;">
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
+
     </div>
+    <hr>
+
     <div class="container">
        <div class="row">
             <div class="col-md-12">
